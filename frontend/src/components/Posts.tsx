507c4c8600a7db8de785cda015f1ca1f83a1defa
Stephanie Chiang <stephanie.chiang@softwire.com>
2025-02-12 12:36:42 +0000

    Working on getting list of posts to show

--- frontend/src/components/Posts.tsx
+++ frontend/src/components/Posts.tsx
@@ -1,18 +1,25 @@
-import React,{useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-export function Posts(){
+export function Posts() {
+    const [allPosts, setAllPosts] = useState({});
 
     useEffect(() => {
         fetch("http://localhost:3001/posts")
-             .then(response => response.json())
-             // .then(data => setMyData(data))
-             .catch(error => console.log('error',error))
-             .finally(()=>console.log('Request finished'))
-             
-    })
-    return(
-        <>
-
-        </>
+        .then(response => setAllPosts(response.json()))
+        // .then(data => setMyData(data))
+        .catch(error => console.log('error',error))
+        .finally(()=>console.log('Request finished'));
+    }, //useEffect depependcy is allPosts?
     )
+
+    if (!allPosts) {
+        return <div>No posts yet.</div>
+    }
+    return (
+        <>
+         <div>
+            {allPosts}
+         </div>
+        </>
+    );
 }
